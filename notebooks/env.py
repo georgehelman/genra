b3050d8b8dd001a069aa37025f50777ee882835e
Imran Shah <shah.imran@epa.gov>
2017-12-20 17:14:05 -0500

    Created mongodb genra_dev_v4 with chm_fp, bio_fp, tox_fp collections Tested out db/fpsim functions Tested out chm/mkfp functions Testing rax/genrapred functions

---
+++ notebooks/env.py
@@ -0,0 +1,38 @@
+%load_ext autoreload
+%autoreload 2
+%pylab inline
+%matplotlib inline
+
+import matplotlib.text as text
+import pandas as pd
+import numpy as np
+import pylab as pl
+import scipy as sp
+import sys
+import rpy2 
+import os 
+
+#Set environment variables
+
+
+# Set up the local source files
+TOP = '/'.join(os.getcwd().split('/')[:-2])+'/'
+#TOP = "/share/home/ishah/ipynb/pb/HTTR_WF/"
+
+LIB = TOP+'lib'
+if not LIB in sys.path: 
+    sys.path.insert(0,LIB)
+
+os.environ['PYTHONPATH']=LIB
+
+
+DAT_DIR = TOP + '/data/'
+FIG_DIR = TOP + '/figs/'
+
+if not os.path.exists(DAT_DIR): os.mkdir(DAT_DIR)
+if not os.path.exists(FIG_DIR): os.mkdir(FIG_DIR)
+
+
+from db.mongo import *
+
+DB = openMongo(db='genra_dev_v4')
