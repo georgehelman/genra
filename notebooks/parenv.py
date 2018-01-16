fb4797b5bcbda147bf27b90dcd0e9a7850720a09
George Helman <helman.george@epa.gov>
2018-01-16 13:08:40 -0500

    Performed parallelism library comparison

--- notebooks/parenv.py
+++ notebooks/parenv.py
@@ -1,8 +1,3 @@
-%reload_ext autoreload
-%autoreload 2
-%pylab inline
-%matplotlib inline
-
 import matplotlib.text as text
 import pandas as pd
 import numpy as np
@@ -11,17 +6,18 @@ import scipy as sp
 import sys
 import os 
 import pymongo
+import importlib
 
 #Set environment variables
 
 
 # Set up the local source files
-TOP = "/share/home/ishah/ipynb/pb/GenRA/"
+TOP = '/'.join(os.getcwd().split('/')[:-1])+'/'
 
 LIB = TOP+'lib'
 if not LIB in sys.path: 
     sys.path.insert(0,LIB)
-
+print(LIB)
 os.environ['PYTHONPATH']=LIB
 
 
@@ -33,8 +29,9 @@ if not os.path.exists(FIG_DIR): os.mkdir(FIG_DIR)
 
 
 from db.mongo import *
+print(importlib.import_module('db.mongo',package='mongo'))
 
 DB = openMongo(db='genra_dev_v4')
-
+print(DB.chm_fp.find_one())
 from db.fpsim import *
-from chm.mkfp import *
+from rax.genrapred import *
