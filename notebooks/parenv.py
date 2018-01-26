e1468eb766c980d275e7a5b5801ed7e6898fae87
George Helman <helman.george@epa.gov>
2018-01-26 12:22:45 -0500

    Wrote parallel code but won't work

--- notebooks/parenv.py
+++ notebooks/parenv.py
@@ -7,9 +7,12 @@ import sys
 import os 
 import pymongo
 import importlib
+from rdkit import Chem as chm
+from rdkit.Chem import Lipinski as lip
+from __future__ import division
 
 #Set environment variables
-
+print('hello')
 
 # Set up the local source files
 TOP = '/'.join(os.getcwd().split('/')[:-1])+'/'
@@ -32,6 +35,16 @@ from db.mongo import *
 print(importlib.import_module('db.mongo',package='mongo'))
 
 DB = openMongo(db='genra_dev_v4')
-print(DB.chm_fp.find_one())
 from db.fpsim import *
 from rax.genrapred import *
+
+def get_phys_fp(compound):
+    c=[]
+    c.append(compound['mol_weight']/500)
+    logp=get_logp(compound['dsstox_sid'])
+    logp= logp/10 if logp else logp
+    c.append(logp)
+    m=chm.MolFromSmiles(compound['smiles'])
+    c.append(lip.NumHDonors(m)/5)
+    c.append(lip.NumHAcceptors(m)/10)
+    return c
