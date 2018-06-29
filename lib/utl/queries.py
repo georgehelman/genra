66c6d97e62753499bbf3f63b7b3df4e11bfa446c
George Helman <helman.george@epa.gov>
2018-06-29 16:33:55 -0400

    Wrote toxref queries

--- lib/utl/queries.py
+++ lib/utl/queries.py
@@ -2,6 +2,7 @@ dsstox='ro_stg_dsstox'
 chemprop='ro_stg_chemprop'
 qsar='ro_stg_qsar'
 invitrodb='dev_invitrodb'
+toxref='dev_toxref_2_0'
 
 
 def get_new_chemicals(l):
@@ -34,7 +35,6 @@ def get_synonyms(sid):
            "INNER JOIN " + dsstox +".generic_substances ON synonyms.fk_generic_substance_id = generic_substances.id " \
            "WHERE dsstox_substance_id='" + sid + "'"
 
-<<<<<<< HEAD
 def get_invitrodb(l):
     in_list= '("'+'","'.join(l)+'")'
     return  "SELECT l5id, level4.l4id, dsstox_substance_id AS dsstox_sid, c_casrn_id AS cas, preferred_name AS name, modl, hitc, fitc, coff, actp, modl_er, modl_tp, modl_ga, modl_gw, modl_la, modl_lw, modl_prob, modl_rmse, modl_acc, modl_acb, modl_ac10, bmad, resp_max, resp_min, max_mean, max_mean_conc, max_med, max_med_conc, logc_max, logc_min, cnst, hill, hcov, gnls, gcov, cnst_er, cnst_aic, cnst_rmse, cnst_prob, hill_tp, hill_tp_sd, hill_ga, hill_ga_sd, hill_gw, hill_gw_sd, hill_er, hill_er_sd, hill_aic, hill_rmse, hill_prob, gnls_tp, gnls_tp_sd, gnls_ga, gnls_ga_sd, gnls_gw, gnls_gw_sd, gnls_la, gnls_la_sd, gnls_lw, gnls_lw_sd, gnls_er,gnls_er_sd, gnls_aic, gnls_rmse, gnls_prob, nconc, npts, nrep, nmed_gtbl, tmpi"\
@@ -43,17 +43,8 @@ def get_invitrodb(l):
             " INNER JOIN " + invitrodb + ".sample ON level4.spid=sample.sa_sample_id"\
             " INNER JOIN " + invitrodb + ".casrn ON sample.sa_gsid=casrn.c_gsid_id"\
             " INNER JOIN " + dsstox + ".generic_substances ON casrn.c_gsid_id=generic_substances.id"\
-	    " WHERE l5id NOT IN " + in_list
+            " WHERE l5id NOT IN " + in_list
 
-=======
-def get_invitrodb():
-    return  "SELECT dsstox_substance_id AS dsstox_sid, c_casrn_id AS cas, preferred_name AS name, modl, hitc, fitc, coff, actp, modl_er, modl_tp, modl_ga, modl_gw, modl_la, modl_lw, modl_prob, modl_rmse, modl_acc, modl_acb, modl_ac10, bmad, resp_max, resp_min, max_mean, max_mean_conc, max_med, max_med_conc, logc_max, logc_min, cnst, hill, hcov, gnls, gcov, cnst_er, cnst_aic, cnst_rmse, cnst_prob, hill_tp, hill_tp_sd, hill_ga, hill_ga_sd, hill_gw, hill_gw_sd, hill_er, hill_er_sd, hill_aic, hill_rmse, hill_prob, gnls_tp, gnls_tp_sd, gnls_ga, gnls_ga_sd, gnls_gw, gnls_gw_sd, gnls_la, gnls_la_sd, gnls_lw, gnls_lw_sd, gnls_er,gnls_er_sd, gnls_aic, gnls_rmse, gnls_prob, nconc, npts, nrep, nmed_gtbl, tmpi"\
-            " FROM " + invitrodb + " .level5"\
-            " INNER JOIN " + invitrodb + ".level4 ON level4.l4id=level5.l4id"\
-            " INNER JOIN " + invitrodb + ".sample ON level4.spid=sample.sa_sample_id"\
-            " INNER JOIN " + invitrodb + ".casrn ON sample.sa_gsid=casrn.c_gsid_id"\
-            " INNER JOIN ro_stg_dsstox.generic_substances ON casrn.c_casrn_id=generic_substances.casrn"
->>>>>>> cronjob
 
 def get_sid_from_cid(cid):
     return "SELECT dsstox_substance_id AS dsstox_sid FROM " + dsstox + ".generic_substances " \
@@ -66,3 +57,28 @@ def get_cid_from_sid(sid):
             "INNER JOIN " + dsstox + ".generic_substance_compounds ON generic_substances.id = generic_substance_compounds.fk_generic_substance_id " \
             "INNER JOIN " + dsstox + ".compounds ON compounds.id = generic_substance_compounds.fk_compound_id " \
             "WHERE dsstox_substance_id='" + sid + "'"
+            
+def get_studies():
+    return "SELECT pod_type,qualifier,pod_value,pod_unit,dose_level,max_dose_level, staggered_dosing, dsstox_substance_id AS dsstox_sid, casrn, preferred_name AS name, life_stage, endpoint_category, endpoint_type, endpoint_target, study_citation, study_year, study_source, data_entry_status,data_entry_level,data_usability,study_type,study_type_guideline,species,strain,admin_route,admin_method,substance_source_name,substance_purity,substance_lot_batch,substance_comment,dose_start,dose_start_unit,dose_end,dose_end_unit,study_comment,processed,ce_eval,fc_adjusted,study_file,received,batch_name "\
+	    "FROM pod JOIN pod_tg_effect USING (pod_id) "\
+            "JOIN chemical USING (chemical_id) "\
+            "JOIN tg_effect USING (tg_effect_id) "\
+            "JOIN effect USING (effect_id) "\
+            "JOIN endpoint USING (endpoint_id) "\
+	    "JOIN study using (study_id) "\
+            "WHERE study_id is not null"
+            
+def get_pods():
+    return "SELECT pod_type,qualifier,pod_value,pod_unit,dose_level,max_dose_level, staggered_dosing, dsstox_substance_id AS dsstox_sid, casrn, preferred_name AS name, life_stage, endpoint_category, endpoint_type, endpoint_target "\
+	    "FROM pod JOIN pod_tg_effect USING (pod_id) "\
+	    "JOIN chemical USING (chemical_id) "\
+	    "JOIN tg_effect USING (tg_effect_id) "\
+	    "JOIN effect USING (effect_id) "\
+	    "JOIN endpoint USING (endpoint_id) "\
+	    "WHERE study_id is null"    
+
+def get_bmds():
+    return "SELECT dsstox_substance_id AS dsstox_sid, casrn, preferred_name AS name, endpoint_category, endpoint_type, endpoint_target, dataset_id,doses_dropped,model_name,model_version,has_output,BMD,BMDL,BMDU,CSF,AIC,pvalue1,pvalue2,pvalue3,pvalue4,Chi2,df,residual_of_interest,warnings,logic_bin,logic_cautions,logic_warnings,logic_failures,recommended,recommended_variable,bmr,bmr_type,study_citation, study_year, study_source, data_entry_status,data_entry_level,data_usability,study_type,study_type_guideline,species,strain,admin_route,admin_method,substance_source_name,substance_purity,substance_lot_batch,substance_comment,dose_start,dose_start_unit,dose_end,dose_end_unit,study_comment,processed,ce_eval,fc_adjusted,study_file,received,batch_name "\
+	    "FROM bmd_models JOIN study USING (study_id) "\
+	    "JOIN chemical USING (chemical_id) "\
+	    "JOIN endpoint USING (endpoint_id)"
